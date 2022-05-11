46efb6e0d41b8b3910fe6dcd8713cada5baf183f
C Bares <colinedwinbares@gmail.com>
2022-05-11 15:57:15 -0500

    refactored to start oscillator in button click listener

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const Tone = window.Tone;
+let osc;
 
 const types = document.querySelectorAll('.typeOption');
 types.forEach(type => {
   type.addEventListener('click', () => {
-    console.log(type.value)
-    osc.type = type.value;
+    if (osc) {
+      osc.type = type.value;
+    }
   })
 });
 
@@ -30,20 +32,24 @@ frequencyEl.addEventListener('change', () => {
   })
 })
 
-const osc = new Tone.Oscillator({
-  type: 'sine',
-  frequency: 440,
-  volume: -5,
-}).toDestination();
-
-
 buttonEl.addEventListener('click', async (e) => {
   if (playing) {
     buttonEl.innerText = 'Start'
     osc.stop();
   } else {
+    buttonEl.innerText = 'Stop';
+    let waveType;
+    types.forEach(type => {
+      if (type.checked) {
+        waveType = type.value;
+      }
+    });
+    osc = new Tone.Oscillator({
+      type: waveType,
+      frequency: frequencyEl.valueAsNumber,
+      volume: volumeEl.valueAsNumber,
+    }).toDestination();
     console.log(osc.get())
-    buttonEl.innerText = 'Stop'
     osc.start();
   }
   playing = !playing;
